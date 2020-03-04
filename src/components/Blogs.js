03143343cc428f8230151d4e16bc23573fa18ede
Andrew Davisson <akdavisson4@gmail.com>
2020-03-03 22:04:55 -0700

    displays blog titles

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,20 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const blogUrl = "http://localhost:3001/blogs";
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
     const fetchData = async () =>{
       const result = await fetch(blogUrl);
       const data = await result.json();
-      console.log(data);
+      setBlogs(data);
     };
     fetchData();
   });
-  
+
   return (
-    <h1>Blog Posts</h1>
+    <div>
+      <h1>Blog Posts</h1>
+      {blogs.map(post => {
+        return <p>{post.title}</p>
+      })}
+    </div>
   );
 }
  
