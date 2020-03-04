6b52e736211fe880a8100535b7650bc24aa6603e
Andrew Davisson <akdavisson4@gmail.com>
2020-03-04 10:01:15 -0700

    Stubbed form and added state

--- src/components/NewBlog.js
+++ src/components/NewBlog.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 const NewBlog = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
   return (
-    <div>
+    <div className="container">
       <h1>New Blog Post</h1>
+      <Form>
+        <Form.Group controlId="formTitle">
+          <Form.Label>Title:</Form.Label>
+          <Form.Control type="text" value={title} onChange={e => setTitle(e.target.value)} placeholder="Enter Title" />
+        </Form.Group>
+        <Form.Group controlId="formContent">
+          <Form.Control as="textarea" value={content} onChange={e => setContent(e.target.value)} placeholder="Write your blog post here" />
+        </Form.Group>
+        <Button variant="primary">Post</Button>
+      </Form>
     </div>
   );
-}
- 
+};
+
 export default NewBlog;
