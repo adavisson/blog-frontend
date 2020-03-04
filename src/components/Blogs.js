435f83edf8d46c63d8de37cbecd5c9cab46044e3
Andrew Davisson <akdavisson4@gmail.com>
2020-03-04 09:31:12 -0700

    Added Cards

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {Card, Button} from 'react-bootstrap';
 
 const blogUrl = "http://localhost:3001/blogs";
 
@@ -18,7 +19,15 @@ const Blogs = () => {
     <div>
       <h1>Blog Posts</h1>
       {blogs.map(post => {
-        return <p>{post.title}</p>
+        return (
+          <Card>
+            <Card.Body>
+              <Card.Title>{post.title}</Card.Title>
+              <Card.Text>{post.content}</Card.Text>
+              <Button variant="primary">View Blog Post</Button>
+            </Card.Body>
+          </Card>
+        )
       })}
     </div>
   );
